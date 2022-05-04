9eec9ed98d1e296b2abfd114ccd7de6157885245
Leobardo Pineda <leobapineda@gmail.com>
2022-05-04 15:53:34 -0500

    finished project

--- html-css/js/App.js
+++ html-css/js/App.js
@@ -5,3 +5,18 @@ const contenedor =  document.querySelector('#contenedor')
 menuButton.addEventListener('click', () =>  {
     contenedor.classList.toggle('active')
 })
+
+const comprobarAncho = () => {
+    if(window.innerWidth <= 768) {
+        contenedor.classList.remove('active')
+    }
+    else {
+        contenedor.classList.add('active')
+    }
+}
+
+comprobarAncho()
+
+    window.addEventListener('resize', () => {
+        comprobarAncho()
+})
