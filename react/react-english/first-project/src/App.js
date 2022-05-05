dc7c45badeb3e736fb0830503d1647d842606e23
Leobardo Pineda <leobapineda@gmail.com>
2022-05-05 16:19:32 -0500

    first commit - first project v1

---
+++ react/react-english/first-project/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Card from './componenets/Card';
+import reactLogo from './img/logo192.png'
+import reactLogoBig from './img/logo512.png'
+
+function App() {
+  return (
+    <div className="App">
+      <Card 
+        logo = {reactLogo}
+      />
+      <img className='react-logo' src={reactLogoBig} alt='freecodecamp bigger logo' />
+    </div>
+  );
+}
+
+export default App;
