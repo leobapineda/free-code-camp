494845816310c703008e140b5b55d3329f4d2bb1
Leobardo Pineda <leobapineda@gmail.com>
2022-05-05 16:58:44 -0500

    cleaner and better syntax

--- react/react-english/first-project/src/App.js
+++ react/react-english/first-project/src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import Card from './componenets/Card';
-import reactLogo from './img/logo192.png'
-import reactLogoBig from './img/logo512.png'
+import "./App.css";
+import Card from "./componenets/Card";
+import reactLogo from "./img/logo192.png";
+import reactLogoBig from "./img/logo512.png";
 
 function App() {
   return (
     <div className="App">
-      <Card 
-        logo = {reactLogo}
+      <Card logo={reactLogo} />
+      <img
+        className="react-logo"
+        src={reactLogoBig}
+        alt="freecodecamp bigger logo"
       />
-      <img className='react-logo' src={reactLogoBig} alt='freecodecamp bigger logo' />
     </div>
   );
 }
--- react/react-english/first-project/src/componenets/Card.jsx
+++ react/react-english/first-project/src/componenets/Card.jsx
@@ -1,29 +1,26 @@
-import '../stylesheet/Card.css'
+import "../stylesheet/Card.css";
 
-function Card({logo}) {
-    return(
-        <div className='card-container' >
-            <header>
-                <p className='header__img-container' ><img src={logo} alt="freecodecamp logo" /> <span>  ReactFacts</span></p>
-                <p>React Course - Project 1</p>
-            </header>
-            <main>
-            <h3>Fun facts about React</h3>
-                <ul>
-                    <li>Was first released in 2013</li>
-                    <li>Was originally created by Jordn Walke</li>
-                    <li>Has well over 100k stars in GitHub</li>
-                    <li>Is it maintained by Facebook</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
-            </main>
-        </div>
-    )
+function Card({ logo }) {
+  return (
+    <div className="card-container">
+      <header>
+        <p className="header__img-container">
+          <img src={logo} alt="freecodecamp logo" /> <span> ReactFacts</span>
+        </p>
+        <p>React Course - Project 1</p>
+      </header>
+      <main>
+        <h3>Fun facts about React</h3>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordn Walke</li>
+          <li>Has well over 100k stars in GitHub</li>
+          <li>Is it maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-export default Card 
-
-// Name: Crayola's Sky Blue
-// Hex: 
-// RGB: (97, 219, 251)
-// CMYK: 0.613, 0.127, 0, 0.015
+export default Card;
