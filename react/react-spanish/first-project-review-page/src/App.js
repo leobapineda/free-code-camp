b49e85b8cce8347ee53d4d4952cb07214122b526
Leobardo Pineda <leobapineda@gmail.com>
2022-05-05 09:19:40 -0500

    info in all cards components

--- react/react-spanish/first-project-review-page/src/App.js
+++ react/react-spanish/first-project-review-page/src/App.js
@@ -10,8 +10,21 @@ function App() {
         city = 'Singapore'
         job = 'Software Engineer'
         company = 'Amazon'
-        text = "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
-      />
+        text = "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life." />
+
+      <Card 
+        name = 'Sarah Chima'
+        city = 'Nigeria'
+        job = 'Software Engineer'
+        company = 'ChatDesk'
+        text = "freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." />
+
+      <Card 
+        name = 'Emma Bostian'
+        city = 'Sweden'
+        job = 'Software Engineer'
+        company = 'Spotify'
+        text = "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." />
     </div>
   );
 }
