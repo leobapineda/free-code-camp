bc8d37f0bd4c96858c3c03e9145a559df5411df3
Leobardo Pineda <leobapineda@gmail.com>
2022-05-06 11:41:09 -0500

    second project v1 part1 finished

--- react/react-english/second-project/src/components/Main.jsx
+++ react/react-english/second-project/src/components/Main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-function Main() {
-    return (
-      <main className="main-body">
-        <h1 className="main-body__h1">Reasons I am excited to learn React</h1>
-        <ol>
-          <li>It is a popular library</li>
-          <li>I'll be able to fit in with he cool kids</li>
-          <li>I am more likely to get a job as a developer if I know React</li>
-        </ol>
-      </main>
-    );
-  }
 
-  export default Main
+function Main() {
+  return (
+    <main className="main-body">
+      <h1 className="main-body__h1">Reasons I am excited to learn React</h1>
+      <ul>
+        <li>It is a popular library</li>
+        <li>I'll be able to fit in with he cool kids</li>
+        <li>I am more likely to get a job as a developer if I know React</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Main;
