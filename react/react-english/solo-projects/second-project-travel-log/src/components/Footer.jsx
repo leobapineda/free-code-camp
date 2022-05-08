bb6ceb7d385b9f502c847fb1c6402794bcee4fad
Leobardo Pineda <leobapineda@gmail.com>
2022-05-08 12:52:52 -0500

    Cleaner and better syntax

--- react/react-english/solo-projects/second-project-travel-log/src/components/Footer.jsx
+++ react/react-english/solo-projects/second-project-travel-log/src/components/Footer.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 // stylesheet
-import '../stylesheet/Footer.css'
+import "../stylesheet/Footer.css";
 export default function Footer() {
-    return(
-        <div className="footer__container" >
-            Made in Leo's House
-        </div>
-    )
+  return <div className="footer__container">Made in Leo's House</div>;
 }
