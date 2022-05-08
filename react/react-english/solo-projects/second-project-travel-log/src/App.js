595dfcae8a0e2495e0c0ce05ace427bc54fe5b2f
Leobardo Pineda <leobapineda@gmail.com>
2022-05-08 11:25:59 -0500

    Card component style

--- react/react-english/solo-projects/second-project-travel-log/src/App.js
+++ react/react-english/solo-projects/second-project-travel-log/src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
      <Header />
      <Card />
+     <Card />
+     <Card />
     </div>
   );
 }
--- react/react-english/solo-projects/second-project-travel-log/src/components/Card.jsx
+++ react/react-english/solo-projects/second-project-travel-log/src/components/Card.jsx
@@ -11,10 +11,11 @@ export default function Card() {
             <img className="card__img" src={iceland} alt="" />
             <div className="card__info" >
                 <div className="card__header" >
-                    <img src={findIcon} alt="" />
+                    <img className="card__icon" src={findIcon} alt="" />
                     <span className="card__country" >Japan </span>
                     <a className="card__link" href="#">View on Google Maps</a>
                 </div>
+                <div className="card__location" >Mount Fuji</div>
                 <div className="card__date" >12 Jan, 2021 - 24 Jan, 2021</div>
                 <div className="card__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non accusamus ipsum, facilis sapiente commodi possimus? Consequatur, dolorum adipisci atque, libero illo quas ad, veritatis porro possimus ratione neque excepturi quisquam.</div>
             </div>
