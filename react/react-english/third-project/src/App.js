13b9f3a0035b3b209248fee85aa04158026ab86c
Leobardo Pineda <leobapineda@gmail.com>
2022-05-09 15:57:07 -0500

    Mapping data from hook useState initial value

--- react/react-english/third-project/src/App.js
+++ react/react-english/third-project/src/App.js
@@ -4,18 +4,34 @@ import Meme from "./components/Meme";
 import "./App.css";
 
 function App() {
-  let [out, setOut] = useState(false);
+  // let [out, setOut] = useState(false);
 
-  function handleClick() {
-    setOut((prevOut) => !prevOut);
-  }
+  // function handleClick() {
+  //   setOut((prevOut) => !prevOut);
+  // }
+
+  // let thingsArray =['t1', 't2']
+  // let parrafoItm = thingsArray.map(item => {
+  //   return(
+  //     <p key={item} >{item}</p>
+  //   )
+  // })
+
+let [thingsArray, setThingsArray] = useState(['t1', 't2'])
+
+let arrayMap = thingsArray.map(item => {
+  return(
+    <p key={item} >{item}</p>
+  )
+})
 
   return (
     <div className="App">
       <Header />
       <Meme />
-      <h1>{out ? "yes" : "no"}</h1>
-      <button onClick={handleClick}>click me</button>
+      {/* <h1>{out ? "yes" : "no"}</h1>
+      <button onClick={handleClick}>click me</button> */}
+      <div>{arrayMap}</div>
     </div>
   );
 }
