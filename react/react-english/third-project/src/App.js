e94e056ca989e2814c522cc14f07abe8e5ef4bcd
Leobardo Pineda <leobapineda@gmail.com>
2022-05-09 10:40:29 -0500

    Getting static data from local data file

--- react/react-english/third-project/src/App.js
+++ react/react-english/third-project/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Meme from './components/Meme';
 
 function App() {
+
   return (
     <div className="App">
       <Header />
--- react/react-english/third-project/src/components/Meme.jsx
+++ react/react-english/third-project/src/components/Meme.jsx
@@ -1,14 +1,40 @@
 import React from "react";
+import memeData from "./memeData";
+
+// atylesheet
 import "../stylesheet/Meme.css";
+
+
 export default function Meme() {
-  // atylesheet
+
+  // export default {
+  //   success: true,
+  //   data: {
+  //     memes: [
+  //       {
+  //         id: "181913649",
+  //         name: "Drake Hotline Bling",
+  //         url: "https://i.imgflip.com/30b1gx.jpg",
+  //         width: 1200,
+  //         height: 1200,
+  //         box_count: 2,
+  //       },
+
+  function getNewImg() {
+    const dataArray = memeData.data.memes
+    let randomIndex = Math.floor(Math.random() * dataArray.length);
+    let dataLink = memeData.data.memes[randomIndex].url
+    console.log(dataLink);
+    console.log(randomIndex);
+  }
+
   return (
     <main className="meme__container">
-      <form action="">
+      <div className="form" >
         <input className="meme__firstInput" type="text" />
         <input className="meme__secondInput" type="text" />
-        <button className="meme__button">Get a new meme 😆</button>
-      </form>
+        <button className="meme__button" onClick={getNewImg} >Get a new meme 😆</button>
+      </div>
     </main>
   );
 }
