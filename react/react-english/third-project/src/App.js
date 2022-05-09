c0ee1b43a1a0236aa583855b0ee3dc75dc6f4ec4
Leobardo Pineda <leobapineda@gmail.com>
2022-05-09 13:54:08 -0500

    Meme generation using useState hook

--- react/react-english/third-project/src/App.js
+++ react/react-english/third-project/src/App.js
@@ -3,23 +3,10 @@ import Header from "./components/Header";
 import Meme from "./components/Meme";
 
 function App() {
-  // const array = ["thing1", "thing2"];
-
-  // function createElement() {
-  //   array.push(`thing${array.length + 1}`);
-  //   console.log(array);
-  // }
-
-  // let arrayElement = array.map((item) => {
-  //   return <p key={item}>{item}</p>;
-  // });
-
   return (
     <div className="App">
       <Header />
       <Meme />
-      {/* {arrayElement}
-      <button onClick={createElement}>Clik me me</button> */}
     </div>
   );
 }
--- react/react-english/third-project/src/components/Meme.jsx
+++ react/react-english/third-project/src/components/Meme.jsx
@@ -5,40 +5,26 @@ import memeData from "./memeData";
 import "../stylesheet/Meme.css";
 
 export default function Meme() {
+  const [img, setImg] = useState("");
 
-  let obj = useState(0)
-  console.log(obj);
-  const [num, setNum] = useState(0)
-
-function sumCount() {
-  setNum((prevNum)  => prevNum + 1)
-}
-
-function substractCount() {
-  setNum((prevNum) => prevNum - 1)
-}
+  function generateUrl() {
+    const memesArray = memeData.data.memes;
+    let randomNumber = Math.floor(Math.random() * memesArray.length);
+    let imgUrl = memesArray[randomNumber].url;
+    setImg(imgUrl);
+  }
 
   return (
     <main className="meme__container">
-      <h1>{num}</h1>
       <div className="form">
         <input className="meme__firstInput" type="text" />
         <input className="meme__secondInput" type="text" />
-        <button className="meme__button" onClick={sumCount}>
-          Get a new meme 1😆
+        <button className="meme__button" onClick={generateUrl}>
+          Get a new meme 1 😆
         </button>
-        <button className="meme__button" onClick={substractCount}>
-        Get a new meme 2😆
-
-        </button>
-
       </div>
       <div className="meme__img-container">
-        <img
-          className="meme__img"
-          src="https://i.imgflip.com/30b1gx.jpg"
-          alt=""
-        />
+        <img className="meme__img" src={img} alt="" />
       </div>
     </main>
   );
